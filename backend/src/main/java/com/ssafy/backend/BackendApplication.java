9b47de16bda840bbc700bc4e983e5079bfb857bd
최수환 <wasdl@naver.com>
2025-05-02 14:18:31 +0900

    feat env 파일 주입 설정 완료

--- backend/src/main/java/com/ssafy/backend/BackendApplication.java
+++ backend/src/main/java/com/ssafy/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend;
 
+import com.ssafy.backend.config.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		DotenvConfig.loadEnv();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
