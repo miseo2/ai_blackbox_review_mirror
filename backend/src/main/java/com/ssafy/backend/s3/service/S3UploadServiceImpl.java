1ea34cee19aed5ef5d5fe0f82209879b3a8c8728
홍해지 <hongs9595@naver.com>
2025-05-14 12:09:42 +0900

    feat: url 만료 시간 관련 코드 수정

--- backend/src/main/java/com/ssafy/backend/s3/service/S3UploadServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/s3/service/S3UploadServiceImpl.java
@@ -132,7 +132,7 @@ public class S3UploadServiceImpl implements S3UploadService {
                 .build();
 
         GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(5))
+                .signatureDuration(Duration.ofSeconds(expirationInSeconds)) 
                 .getObjectRequest(getObjectRequest)
                 .build();
 
